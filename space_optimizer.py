58754898fe32f47b38b0054aa6958c1373ee575f
Bulat Akhmatov <b.akhmatov@innopolis.university>
2024-04-07 22:30:57 +0300

    space optimizer sceleton

---
+++ space_optimizer.py
@@ -0,0 +1,73 @@
+import os
+import random
+
+
+def generate_initial_population(parameters, population_size):
+    population = []
+    # parameters = [['buy_volumeAVG', 'int'], ['buy_rsi', 'float']]
+    for i in range(population_size):
+        candidate = {}
+        for j in range(len(parameters)):
+            if parameters[j][1] == 'int':
+                candidate[parameters[j][0]]['low'] = random.randint(0, 10000)
+                candidate[parameters[j][0]]['high'] = random.randint(0, 100000)
+                candidate[parameters[j][0]]['default'] = random.randint(0, 10000)
+            elif parameters[j][1] == 'float':
+                candidate[parameters[j][0]]['low'] = random.uniform(0, 10000)
+                candidate[parameters[j][0]]['high'] = random.uniform(0, 100000)
+                candidate[parameters[j][0]]['default'] = random.uniform(0, 10000)
+                candidate[parameters[j][0]]['decimals'] = random.randint(0, round(
+                    100000 / candidate[parameters[j][0]]['high']))
+        population.append(candidate)
+    return population
+
+
+def evaluate_candidate(candidate_class, loss_function):
+    loss = 0
+    os.system(f"freqtrade hyperopt --strategy {candidate_class} --hyperopt-loss {loss_function} > result.txt")
+    with open("result.txt", "r") as file:
+        for line in file:
+            if "loss" in line:
+                loss = float(line.split(":")[1])
+    return loss
+
+
+def mutate_candidate(candidate):
+    mutated_candidate = candidate
+    for key in mutated_candidate.keys():
+        if type(mutated_candidate[key]) == dict:
+            if 'low' in mutated_candidate[key].keys():
+                mutated_candidate[key]['low'] = random.randint(0, 10000)
+            if 'high' in mutated_candidate[key].keys():
+                mutated_candidate[key]['high'] = random.randint(0, 100000)
+            if 'default' in mutated_candidate[key].keys():
+                mutated_candidate[key]['default'] = random.randint(0, 10000)
+            if 'decimals' in mutated_candidate[key].keys():
+                mutated_candidate[key]['decimals'] = random.randint(0, round(100000 / mutated_candidate[key]['high']))
+    return mutated_candidate
+
+
+def crossover_candidates(candidate1, candidate2):
+    crossover_point = random.randint(0, len(candidate1.keys()))
+    new_candidate = {}
+    for i, key in enumerate(candidate1.keys()):
+        if i < crossover_point:
+            new_candidate[key] = candidate1[key]
+        else:
+            new_candidate[key] = candidate2[key]
+    return new_candidate
+
+
+def genetic_algorithm(parameters, population_size, generations, loss_function):
+    population = generate_initial_population(parameters, population_size)
+    for i in range(generations):
+        for j in range(len(population)):
+            population[j]['loss'] = evaluate_candidate(get_class(population[j]), loss_function)
+        population = sorted(population, key=lambda x: x['loss'])
+        new_population = []
+        for j in range(population_size // 2):
+            new_population.append(population[j])
+            new_population.append(mutate_candidate(population[j]))
+            new_population.append(crossover_candidates(population[j], population[j + 1]))
+        population = new_population
+    return population[0]
