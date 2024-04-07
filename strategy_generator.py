1421f67c3f46137a85b41511fb30485998023d05
sasha <alexandravabnits@gmail.com>
2024-04-08 00:08:29 +0300

    fix strategy_generator.py

--- strategy_generator.py
+++ strategy_generator.py
@@ -9,36 +9,7 @@ import pickle
 from user_data.strategies.diamond_strategy import Diamond
 
 
-class NewStrategy(IStrategy):
 
-    def __init__(self, strategy: Type[IStrategy], parameters: List[Any]):
-        self.strategy = strategy
-        self.parameters = parameters
-
-    class HyperOpt:
-        # Define a custom stoploss space.
-        def stoploss_space(self):
-            return [SKDecimal(-0.05, -0.01, decimals=3, name='stoploss')]
-
-        # Define custom ROI space
-        def roi_space(self) -> List[Dimension]:
-            return [
-                Integer(10, 120, name='roi_t1'),
-                Integer(10, 60, name='roi_t2'),
-                Integer(10, 40, name='roi_t3'),
-                SKDecimal(0.01, 0.04, decimals=3, name='roi_p1'),
-                SKDecimal(0.01, 0.07, decimals=3, name='roi_p2'),
-                SKDecimal(0.01, 0.20, decimals=3, name='roi_p3'),
-            ]
-
-        def generate_roi_table(self, params: Dict) -> Dict[int, float]:
-            roi_table = {}
-            roi_table[0] = params['roi_p1'] + params['roi_p2'] + params['roi_p3']
-            roi_table[params['roi_t3']] = params['roi_p1'] + params['roi_p2']
-            roi_table[params['roi_t3'] + params['roi_t2']] = params['roi_p1']
-            roi_table[params['roi_t3'] + params['roi_t2'] + params['roi_t1']] = 0
-
-            return roi_table
 class Generator:
 
     def __init__(self):
@@ -48,11 +19,11 @@ class Generator:
         class NewStrategy(strategy):
             class HyperOpt:
                 # Define a custom stoploss space.
-                def stoploss_space():
+                def stoploss_space(self):
                     return [SKDecimal(-0.05, -0.01, decimals=3, name='stoploss')]
 
                 # Define custom ROI space
-                def roi_space() -> List[Dimension]:
+                def roi_space(self) -> List[Dimension]:
                     return [
                         Integer(10, 120, name='roi_t1'),
                         Integer(10, 60, name='roi_t2'),
@@ -92,6 +63,3 @@ if __name__ == '__main__':
     generator = Generator()
     new_strategy = generator.generate(Diamond, [])
     # save to file
-    generator.
-
-
